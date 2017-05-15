1280dde43e7cdd75bc54b82abd83728788121ff5
oslc15 <lifh.oscar@gmail.com>
2017-05-15 20:39:53 +0100

    Test case for user story 1

---
+++ Lab_5_TDD/src/Test.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.*;
+
+public class Test {
+
+	@org.junit.Test
+	public void frameTest() throws Exception {
+		//Create gameObject
+		Game game = new Game();
+		
+		//throwing bowling balls
+		game.bowlingThrow(4);
+		game.bowlingThrow(5);
+		
+		//total value of the specific frame
+		int frame = game.getSpecificFrame(0);
+		
+		//A single frame is not allowed to be greater or less than 10
+		assertTrue(0 >= frame && frame <= 10);
+		assertEquals(frame, 9);
+	}
+
+}
